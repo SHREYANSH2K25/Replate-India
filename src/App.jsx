92de42eb5bd7abab57d3f04e8e9b6c9ef388e362
Shreyansh Jain <195452748+SHREYANSH2K25@users.noreply.github.com>
2025-10-03 22:24:12 +0530

    navigation set

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './componenets/Home/Home'
+import Browse from './componenets/Browse/Browse'
+import Post from './componenets/Post/Post'
+import Dashboard from './componenets/Dashboard/Dashboard'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Food Management</h1>
-    </>
+    <Router>
+      <div className="min-h-screen bg-teal-50">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
